68cb3e120dbcb83e614d37734a42fc118e7390df
adityapotdar11 <aditya@webstersolutions.in>
2023-10-03 16:27:54 +0530

    Functional working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ jQuery(function () {
                 }
             },
         });
+    loadTodoList();
 });
 
 document
@@ -18,6 +19,89 @@ document
         event.preventDefault();
     });
 
-function submitForm(){
-    
+function submitForm() {
+    const task = document.getElementById("todo-textbox").value;
+    const payload = {
+        task: task,
+        completed: false,
+    };
+    if (localStorage.todolist) {
+        let localData = JSON.parse(localStorage.todolist);
+        localData.push(payload);
+        localStorage.todolist = JSON.stringify(localData);
+    } else {
+        let localData = [];
+        localData.push(payload);
+        localStorage.todolist = JSON.stringify(localData);
+    }
+    document.getElementById("todo-textbox").value = "";
+    loadTodoList();
+    alert("Task added.");
+}
+
+function loadTodoList() {
+    let html = "There are no tasks.";
+    if (localStorage.todolist) {
+        let localData = JSON.parse(localStorage.todolist);
+        if (localData.length) {
+            html = "";
+            localData.forEach((element, key) => {
+                if (element.completed == true) {
+                    html += `<li class="todo-element" id="main-${key}">
+                    <span class="checkbox-parent">
+                    <input type="checkbox" onchange="toggleComplete('${key}')" checked/>
+                    </span>
+                    <span class="task-name-parent completed" id="task-name-${key}">${element.task}</span>
+                    <span class="task-remove-parent">
+                    <a class="remove" onclick="removeTask('${key}')"><i data-feather="x-circle"></i></a>
+                    </span>
+                    </li>`;
+                } else {
+                    html += `<li class="todo-element" id="main-${key}">
+                    <span class="checkbox-parent">
+                    <input type="checkbox" onchange="toggleComplete('${key}')"/>
+                    </span>
+                    <span class="task-name-parent" id="task-name-${key}">${element.task}</span>
+                    <span class="task-remove-parent">
+                    <a class="remove" onclick="removeTask('${key}')"><i data-feather="x-circle"></i></a>
+                    </span>
+                    </li>`;
+                }
+            });
+        }
+    }
+    document.getElementById("todo-list-ul").innerHTML = html;
+    feather.replace();
+}
+
+function toggleComplete(taskId) {
+    if (localStorage.todolist) {
+        let localData = JSON.parse(localStorage.todolist);
+        if (localData.length) {
+            if (localData[taskId]["completed"]) {
+                localData[taskId]["completed"] = false;
+                document
+                    .getElementById(`task-name-${taskId}`)
+                    .classList.remove("completed");
+            } else {
+                localData[taskId]["completed"] = true;
+                document
+                    .getElementById(`task-name-${taskId}`)
+                    .classList.add("completed");
+            }
+        }
+        localStorage.todolist = JSON.stringify(localData);
+    }
+}
+
+function removeTask(taskId) {
+    if (localStorage.todolist) {
+        let localData = JSON.parse(localStorage.todolist);
+        if (localData.length) {
+            localData.splice(taskId, 1);
+        }
+        localStorage.todolist = JSON.stringify(localData);
+        loadTodoList();
+    }
+    alert("Task removed.");
 }
